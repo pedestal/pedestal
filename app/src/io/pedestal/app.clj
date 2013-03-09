90899b161e8fdda741ed9b7976da30f7c66db396
Brenton Ashworth <brenton@thinkrelevance.com>
2013-03-09 15:25:54 -0800

    change event to message in io.pedestal.app

--- app/src/io/pedestal/app.clj
+++ app/src/io/pedestal/app.clj
@@ -6,13 +6,13 @@
 
     Application behavior is defined in terms of five funcitons:
 
-    * model - reduce event streams into a value
+    * model - reduce message streams into a value
 
-    * output - generate events to send to external services
+    * output - generate messages to send to external services
 
-    * view - transform and combine event streams (models) in arbitrary dataflows
+    * view - transform and combine message streams (models) in arbitrary dataflows
 
-    * events - generate new events - supporting event composition
+    * events - generate new input messages
 
     * emitter - generate changes to the application model
     "
@@ -30,7 +30,7 @@
 ;; Default functions
 ;; ================================================================================
 
-(defn default-output-fn [event old-model new-model]
+(defn default-output-fn [message old-model new-model]
   nil)
 
 (defn default-view-fn [state input-name old new]
@@ -186,23 +186,23 @@
   (let [{:keys [name]} message]
     (update-in state [:named-paths] dissoc name)))
 
-(defn get-receiver [event]
-  (let [to (msg/topic event)]
+(defn get-receiver [message]
+  (let [to (msg/topic message)]
     (if (keyword? to)
       to
       (or (:node to) (:model to) (:service to)))))
 
-(defn run-model [state flow model-name event]
+(defn run-model [state flow model-name message]
   (assert (get-in flow [:models model-name])
-          (str "Model with name " model-name " does not exist. Message is " event))
+          (str "Model with name " model-name " does not exist. Message is " message))
   (let [model-fn (get-in flow [:models model-name])]
-    (update-in state [:models model-name] model-fn event)))
+    (update-in state [:models model-name] model-fn message)))
 
-(defn run-output [state old-state flow model-name event]
+(defn run-output [state old-state flow model-name message]
   (let [output-fn (get-in flow [:model->output model-name])
         old-model (get-in old-state [:models model-name])
         new-model (get-in state [:models model-name])
-        out (when output-fn (output-fn event old-model new-model))
+        out (when output-fn (output-fn message old-model new-model))
         out (if (vector? out) {:output out} out)]
     (if out
       (-> state
@@ -252,9 +252,9 @@
 (defn run-events [state old-state flow modified-inputs]
   (reduce (fn [s [view-name {:keys [old new]}]]
             (let [event-fn (get-in flow [:view->events view-name])
-                  events (event-fn view-name old new)]
-              (if events
-                (update-in s [:events] into events)
+                  messages (event-fn view-name old new)]
+              (if messages
+                (update-in s [:events] into messages)
                 s)))
           state
           modified-inputs))
@@ -286,14 +286,14 @@
             n-views)))
 
 (defn- run-dataflow
-  "Starting with the given input event, run the dataflow producing a
+  "Starting with the given input message, run the dataflow producing a
   new state."
-  [state flow event]
+  [state flow message]
   (let [old-state state
-        model-name (get-receiver event)
+        model-name (get-receiver message)
         new-state (-> state
-                      (run-model flow model-name event)
-                      (run-output old-state flow model-name event)
+                      (run-model flow model-name message)
+                      (run-output old-state flow model-name message)
                       (run-views old-state flow model-name :models))
         modified-views (find-modified-inputs :views old-state new-state)
         modified-models (find-modified-inputs :models old-state new-state)
@@ -301,8 +301,8 @@
                    (run-events old-state flow modified-views)
                    (run-emitters old-state flow (merge modified-views modified-models)))]
     (if (not (empty? (:events result)))
-      (reduce (fn [s event]
-                (run-dataflow (assoc s :events []) flow event))
+      (reduce (fn [s message]
+                (run-dataflow (assoc s :events []) flow message))
               result
               (:events result))
       result)))
@@ -322,8 +322,8 @@
                         (some (fn [s] (path-starts-with? path s)) subscriptions)))
                   (mapcat tree/expand-map deltas)))))
 
-(defn process-event
-  "Using the given flow, process the given event producing a new
+(defn process-message
+  "Using the given flow, process the given message producing a new
   state."
   [state flow message]
   (let [old-state state
@@ -340,14 +340,14 @@
     :output []
     :events []))
 
-(defn transact-one [state flow event]
-  (process-event (assoc (pre-tx-state state) :input event) flow event))
+(defn transact-one [state flow message]
+  (process-message (assoc (pre-tx-state state) :input message) flow message))
 
-(defn transact-many [state flow events]
-  (reduce (fn [a event]
-            (process-event (assoc a :input event) flow event))
+(defn transact-many [state flow messages]
+  (reduce (fn [a message]
+            (process-message (assoc a :input message) flow message))
           (pre-tx-state state)
-          events))
+          messages))
 
 
 ;; Build and interface with the outside world
@@ -383,7 +383,7 @@
   output queues for sending and receiving messages.
 
   The description map contains a subset of the keys:
-  :default-emitter, :models, :output, :views, :events and :emitters."
+  :models, :output, :views, :events, :emitters and :navigation."
   [description]
   (let [app-atom (atom {:output [] :events []})
         flow (make-flow description)
@@ -444,8 +444,8 @@
 (defn run! [app script]
   (assert (or (vector? script) (list? script)) "The passed script must be a vector or list")
   (assert (every? map? script) "Each element of the passed script must be a map")
-  (doseq [event script]
-    (p/put-message (:input app) event)))
+  (doseq [message script]
+    (p/put-message (:input app) message)))
 
 
 ;; Queue consumers
