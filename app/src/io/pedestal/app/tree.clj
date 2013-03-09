8daf6dff1c76eec624b02404b768be7ac326506f
Brenton Ashworth <brenton@thinkrelevance.com>
2013-03-09 13:50:16 -0800

    improved docs that refer to events

--- app/src/io/pedestal/app/tree.clj
+++ app/src/io/pedestal/app/tree.clj
@@ -4,20 +4,18 @@
     "A tree structure which can be used to represent the user
      interface of an application.
 
-    A tree node may contain a value, attributes and events.
+    A tree node may contain a value, attributes and data model transformations.
 
     Values are arbitrary Clojure data.
 
     Attributes are a map of keys to values.
 
-    Events are maps of event names to collections of data which
-    represent the runtime events/messages which can be sent to an
-    application's Behavior.
+    Data model transformations are maps of transform names to
+    collections of messages which can update an application's data
+    model.
 
     The purpose of this tree is provide an information model for a
-    user interface. The data in this tree are all that is required to
-    render the user interface. The tree contains no information about
-    how the user interface should be rendered."
+    application."
     (:require [clojure.set :as set]
               [io.pedestal.app.util.log :as log]
               [io.pedestal.app.query :as query]))
