c7839c8055105dd809ddcb55123932827541eda1
Gabriel Horner <gabriel.horner@gmail.com>
2013-03-15 13:38:12 -0400

    update remaining libraries not depending on 1.5.0

--- tomcat/project.clj
+++ tomcat/project.clj
@@ -1,6 +1,6 @@
 (defproject io.pedestal/pedestal.tomcat "0.0.1-SNAPSHOT"
   :description "Embedded Tomcat adapter for Pedestal HTTP Service"
-  :dependencies [[org.clojure/clojure "1.5.0-RC16"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.apache.tomcat.embed/tomcat-embed-logging-juli "7.0.30"]
                  [org.apache.tomcat.embed/tomcat-embed-jasper "7.0.30"]
                  [org.apache.tomcat.embed/tomcat-embed-core "7.0.30"]
