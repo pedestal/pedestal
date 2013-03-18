0cb9b3a5e5e665907762a0de9d96decbd243233e
Alex Redington <alex.redington@thinkrelevance.com>
2013-03-18 13:54:25 -0400

    Pedestal Version 0.1.0 ready for release

--- service-template/src/leiningen/new/pedestal_service/project.clj
+++ service-template/src/leiningen/new/pedestal_service/project.clj
@@ -1,14 +1,14 @@
-(defproject {{name}} "0.1.0-SNAPSHOT"
+(defproject {{name}} "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [io.pedestal/pedestal.service "0.1.0-SNAPSHOT"]
+                 [io.pedestal/pedestal.service "0.1.0"]
 
                  ;; Remove this line and uncomment the next line to
                  ;; use Tomcat instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.1.0-SNAPSHOT"]
+                 [io.pedestal/pedestal.jetty "0.1.0"]
                  ;; [io.pedestal/pedestal.tomcat "0.0.1-SNAPSHOT"]
 
                  ;; Logging
