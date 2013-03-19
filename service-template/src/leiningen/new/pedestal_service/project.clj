2141e18a464b46020ce435e9314effe5e81d2652
Gabriel Horner <gabriel.horner@gmail.com>
2013-03-19 05:21:27 -0400

    add lein2 to templates since that is required

--- service-template/src/leiningen/new/pedestal_service/project.clj
+++ service-template/src/leiningen/new/pedestal_service/project.clj
@@ -17,5 +17,6 @@
                  [org.slf4j/jcl-over-slf4j "1.7.2"]
                  [org.slf4j/log4j-over-slf4j "1.7.2"]]
   :profiles {:dev {:source-paths ["dev"]}}
+  :min-lein-version "2.0.0"
   :resource-paths ["config"]
   :main ^{:skip-aot true} {{name}}.server)
