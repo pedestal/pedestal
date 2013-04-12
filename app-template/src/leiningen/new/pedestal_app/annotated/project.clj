8441d46a5f097b52578dae57dd05b001a05531e2
Alex Redington <alex.redington@thinkrelevance.com>
2013-04-12 10:00:50 -0400

    Running `lein clean` should delete the "out" directory

--- app-template/src/leiningen/new/pedestal_app/annotated/project.clj
+++ app-template/src/leiningen/new/pedestal_app/annotated/project.clj
@@ -10,4 +10,5 @@
   :min-lein-version "2.0.0"
   :source-paths ["app/src" "app/templates"]
   :resource-paths ["config"]
+  :target-path "out/"
   :aliases {"dumbrepl" ["trampoline" "run" "-m" "clojure.main/main"]})
