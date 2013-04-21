d1b470f018781a4b4c6a62a2321c3f7d39ca5029
Brenton Ashworth <brenton@thinkrelevance.com>
2013-04-21 15:11:08 -0400

    move Clojure tests under test/clj

--- app/project.clj
+++ app/project.clj
@@ -17,5 +17,7 @@
                  [ch.qos.logback/logback-classic "1.0.6"]
                  [enlive "1.0.0" :exclusions [org.clojure/clojure]]
                  [domina "1.0.1"]]
+  :test-paths ["test/clj"]
+  :plugins [[lein-marginalia "0.7.1"]]
   :profiles {:dev {:source-paths ["dev"]}}
   :aliases {"dumbrepl" ["trampoline" "run" "-m" "clojure.main/main"]})
