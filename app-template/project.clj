5c87a5870369101b68d0dbeae24c83b152cb3a10
Alex Redington <alex.redington@thinkrelevance.com>
2013-05-03 13:38:06 -0400

    Start 0.1.7-SNAPSHOT development stream

--- app-template/project.clj
+++ app-template/project.clj
@@ -9,7 +9,7 @@
 ;
 ; You must not remove this notice, or any other, from this software.
 
-(defproject pedestal-app/lein-template "0.1.6"
+(defproject pedestal-app/lein-template "0.1.7-SNAPSHOT"
   :description "A Pedestal Application template."
   :min-lein-version "2.0.0"
   :eval-in-leiningen true)
