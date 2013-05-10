ad2b8461c43a467967a88fd5b7e57a1258c3e7bb
Ryan Neufeld <ryan@rkn.io>
2013-05-10 13:13:26 -0400

    Cleanup logback-classic dependencies and exclusions. Fixes #55

--- service-template/src/leiningen/new/pedestal_service/project.clj
+++ service-template/src/leiningen/new/pedestal_service/project.clj
@@ -12,7 +12,7 @@
                  ;; [io.pedestal/pedestal.tomcat "0.1.7-SNAPSHOT"]
 
                  ;; Logging
-                 [ch.qos.logback/logback-classic "1.0.7"]
+                 [ch.qos.logback/logback-classic "1.0.7" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.2"]
                  [org.slf4j/jcl-over-slf4j "1.7.2"]
                  [org.slf4j/log4j-over-slf4j "1.7.2"]]
