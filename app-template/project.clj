de0e0ba504f2efbbf59d80d044643495be5ee784
Ryan Neufeld <ryan@rkn.io>
2013-05-24 16:13:33 -0400

    Prepare 0.1.7 release

--- app-template/project.clj
+++ app-template/project.clj
@@ -9,7 +9,7 @@
 ;
 ; You must not remove this notice, or any other, from this software.
 
-(defproject pedestal-app/lein-template "0.1.7-SNAPSHOT"
+(defproject pedestal-app/lein-template "0.1.7"
   :description "A Pedestal Application template."
   :min-lein-version "2.0.0"
   :eval-in-leiningen true)
