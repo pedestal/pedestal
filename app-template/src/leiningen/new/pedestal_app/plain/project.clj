a6245506ebeaab174b6c7ec918316741561fc291
Stuart Sierra <mail@stuartsierra.com>
2013-06-06 10:52:57 -0400

    Update all dependencies from Clojure 1.5.0 to 1.5.1
    Clojure 1.5.0 was a buggy release, fixed in 1.5.1

--- app-template/src/leiningen/new/pedestal_app/plain/project.clj
+++ app-template/src/leiningen/new/pedestal_app/plain/project.clj
@@ -1,6 +1,6 @@
 (defproject {{raw-name}} "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1586"]
                  [domina "1.0.1"]
                  [ch.qos.logback/logback-classic "1.0.7" :exclusions [org.slf4j/slf4j-api]]
--- service-template/src/leiningen/new/pedestal_service/project.clj
+++ service-template/src/leiningen/new/pedestal_service/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [io.pedestal/pedestal.service "0.1.9-SNAPSHOT"]
 
                  ;; Remove this line and uncomment the next line to
