12f4e380104236a64fa92a21afb6cff7b4fd8df7
Ryan Neufeld <ryan@rkn.io>
2013-06-14 14:33:02 -0400

    Prepare 0.1.9 release

--- service-template/src/leiningen/new/pedestal_service/project.clj
+++ service-template/src/leiningen/new/pedestal_service/project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [io.pedestal/pedestal.service "0.1.9-SNAPSHOT"]
+                 [io.pedestal/pedestal.service "0.1.9"]
 
                  ;; Remove this line and uncomment the next line to
                  ;; use Tomcat instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.1.9-SNAPSHOT"]
-                 ;; [io.pedestal/pedestal.tomcat "0.1.9-SNAPSHOT"]
+                 [io.pedestal/pedestal.jetty "0.1.9"]
+                 ;; [io.pedestal/pedestal.tomcat "0.1.9"]
 
                  ;; auto-reload changes
                  [ns-tracker "0.2.1"]
