fa3ad1dd0eecd91b11f013b00d18b06245ce2745
Ryan Neufeld <ryan@rkn.io>
2013-09-01 11:00:32 -0400

    Begin 0.2.2-SNAPSHOT development

--- service-template/src/leiningen/new/pedestal_service/project.clj
+++ service-template/src/leiningen/new/pedestal_service/project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [io.pedestal/pedestal.service "0.2.1"]
-                 [io.pedestal/pedestal.service-tools "0.2.1"]
+                 [io.pedestal/pedestal.service "0.2.2-SNAPSHOT"]
+                 [io.pedestal/pedestal.service-tools "0.2.2-SNAPSHOT"]
 
                  ;; Remove this line and uncomment the next line to
                  ;; use Tomcat instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.2.1"]
-                 ;; [io.pedestal/pedestal.tomcat "0.2.1"]
+                 [io.pedestal/pedestal.jetty "0.2.2-SNAPSHOT"]
+                 ;; [io.pedestal/pedestal.tomcat "0.2.2-SNAPSHOT"]
                  ]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
