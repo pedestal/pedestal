318a5ad72bca884f0f4520a0abb3da0bf201f4de
Gabriel Horner <gabriel.horner@gmail.com>
2013-11-01 15:26:32 -0400

    first pass at docstrings for construct

--- app/src/io/pedestal/app/construct.clj
+++ app/src/io/pedestal/app/construct.clj
@@ -16,7 +16,10 @@
             [io.pedestal.app.route :as route])
   (:use [clojure.core.async :only [chan put!]]))
 
-(defn- router-transforms [_ inform-message]
+(defn- router-transforms
+  "Given an inform with :channel-added and :channel-removed events, generates
+  ::router transforms the router can process."
+  [_ inform-message]
   (mapv (fn [[ _ event c config]]
           (cond (= event :channel-added)
                 [[[::router] :add [c config :*]]]
@@ -24,7 +27,30 @@
                 [[[::router] :remove [c config :*]]]))
         inform-message))
 
-(defn build [init-model config]
+(defn build
+  "Given an initial model and an app config, construct an app and return its
+  inform channel. An app's info model lives at [:info]. Each app has a router
+  defined at [::router] that handles routing all transforms.
+
+  When an app receives an inform, it produces transforms and sends them to its
+  router. To handle an app's UI or external services, it is recommended to
+  generate transforms to a separate path e.g. [:out] and add a channel to the
+  app's router. For example:
+
+  (let [cin (construct/build {:info {}} {:in [] :out []})
+        cout (chan 10)]
+    (put! cin [[[::app/router] :channel-added cout [:out :**]]]))
+
+  An app config has the following config keys:
+
+  * :in: This config vector is used by mapper/inform->transforms to generate
+    inbound transforms to the router from an external inform. This is required.
+  * :flow: This config vector is used by flow/transform->inform to generate
+    :info informs from a router transform.  This is optional. If not present,
+    model/transform->inform generates the informs.
+  * :out: This config vector is used by mapper/inform->transforms to generate
+    outbound transforms to the router from a model/flow inform. This is required."
+  [init-model config]
   ;; TODO: How would we configure custom arg-fns for each mapper?
   ;; maybe the answer is: if you need to do that then wire it up yourself
   (let [in-config (conj (:in config) [router-transforms [::router] :*])
