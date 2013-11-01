578a479093ea4c7e3b407bc7fa434af432e93f3d
Brenton Ashworth <brenton@thinkrelevance.com>
2013-11-01 09:24:35 -0400

    add test with square root example

--- app/test/clj/io/pedestal/app/construct_test.clj
+++ app/test/clj/io/pedestal/app/construct_test.clj
@@ -40,3 +40,51 @@
       (put! cin [[[:button :inc] :click]])
       (let [x (set (helpers/take-n 1 cout))]
         (is (= x #{[[[:out :a] :updated {:info {:a 0 :b 0}} {:info {:a 1 :b 0}}]]}))))))
+
+;; Calculate square root using Heron's method
+;; ================================================================================
+
+(defn abs [x]
+  (Math/abs (double x)))
+
+(defn sr-input [_ [[_ _ {:keys [x guess accuracy] :as init}]]]
+  ;; TODO: this doesn't work if we use
+  ;; [[:info] (constantly init)]
+  ;; I think this is because changes are only reported to :info
+  #_[[[[:info] (constantly init)]]]
+  [[[[:info] assoc :x x]
+    [[:info] assoc :guess guess]
+    [[:info] assoc :accuracy accuracy]]])
+
+(defn divide [_ [[_ _ _ model]]]
+  [[[[:info :divide] (constantly (/ (get-in model [:info :x])
+                                    (get-in model [:info :guess])))]]])
+
+(defn sum [_ [[_ _ _ model]]]
+  [[[[:info :sum] (constantly (+ (get-in model [:info :divide])
+                                 (get-in model [:info :guess])))]]])
+
+(defn half [_ [[_ _ _ model]]]
+  [[[[:info :half] (constantly (/ (get-in model [:info :sum]) 2))]]])
+
+(defn good-enough? [_ [[_ _ {{ohalf :half} :info} {{acc :accuracy nhalf :half} :info}]]]
+  (when (and ohalf nhalf acc (< acc (abs (- nhalf ohalf))))
+    [[[[:info :guess] (constantly (- nhalf acc))]]]))
+
+(defn sr-output [_ [[_ _ _ model]]]
+  [[[[:out] :set-result (get-in model [:info :half])]]])
+
+(deftest square-root-test
+  (let [accuracy 0.000001
+        config {:in [[sr-input [:user] :start]]
+                :flow [[divide [:info :x] :* [:info :guess] :*]
+                       [sum [:info :guess] :* [:info :divide] :*]
+                       [half [:info :sum] :*]]
+                :out [[good-enough? [:info :half] :* [:info :accuracy] :*]
+                      [sr-output [:info :half] :*]]}
+        ichan (build {:info {:guess 0 :x 0 :accuracy 0 :divide 0 :sum 0 :half 0}} config)
+        tchan (chan 10)]
+    (put! ichan [[[::app/router] :channel-added tchan [:out :**]]])
+    (put! ichan [[[:user] :start {:x 42 :guess 7 :accuracy accuracy}]])
+    (let [[[ _ _ answer]] (last (helpers/take-n 4 tchan))]
+      (is (<= (abs (- answer 6.48074069840786)) accuracy)))))
