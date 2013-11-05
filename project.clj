3c44686275fcad89e2cfdea17f3230c64dd41a6d
Ryan Neufeld <ryan@rkn.io>
2013-11-05 12:59:26 -0500

    Prepare 0.2.2 release

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
 ;
 ; You must not remove this notice, or any other, from this software.
 
-(defproject io.pedestal/pedestal "0.2.2-SNAPSHOT"
+(defproject io.pedestal/pedestal "0.2.2"
   :plugins [[lein-sub "0.2.3"]]
   :sub ["service"
         "jetty"
--- tomcat/project.clj
+++ tomcat/project.clj
@@ -9,7 +9,7 @@
 ;
 ; You must not remove this notice, or any other, from this software.
 
-(defproject io.pedestal/pedestal.tomcat "0.2.2-SNAPSHOT"
+(defproject io.pedestal/pedestal.tomcat "0.2.2"
   :description "Embedded Tomcat adapter for Pedestal HTTP Service"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.apache.tomcat.embed/tomcat-embed-logging-juli "7.0.30"]
