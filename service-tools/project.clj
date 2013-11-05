965ac95257e3ec1de2416962ca5d231e8df39e26
Ryan Neufeld <ryan@rkn.io>
2013-11-05 13:02:08 -0500

    Begin 0.2.3-SNAPSHOT development.

--- service-tools/project.clj
+++ service-tools/project.clj
@@ -9,10 +9,10 @@
 ;
 ; You must not remove this notice, or any other, from this software.
 
-(defproject io.pedestal/pedestal.service-tools "0.2.2"
+(defproject io.pedestal/pedestal.service-tools "0.2.3-SNAPSHOT"
   :description "Pedestal tools for service development"
   :min-lein-version "2.0.0"
-  :dependencies [[io.pedestal/pedestal.service "0.2.2"]
+  :dependencies [[io.pedestal/pedestal.service "0.2.3-SNAPSHOT"]
 
                  ;; Auto-reload changes
                  [ns-tracker "0.2.1"]
