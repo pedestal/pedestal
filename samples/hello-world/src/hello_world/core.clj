c3630230f69f4842191b7561b911d78644147baf
Ryan Neufeld <ryan@rkn.io>
2014-07-19 20:55:38 -0500

    Reproduce 'async handling in 0.3.0' exception

--- samples/hello-world/src/hello_world/core.clj
+++ samples/hello-world/src/hello_world/core.clj
@@ -1,13 +1,16 @@
 (ns hello-world.core
   (:require [io.pedestal.http.route.definition :refer [defroutes]]
-            [io.pedestal.http :as http]))
-
+            [io.pedestal.http :as http]
+            [clojure.core.async :as async]))
 
 (defn hello-world [req]
-  (let [name (get-in req [:params :name] "World")]
-    {:status 200
-     :body (str "Hello, " name "!")
-     :headers {}}))
+  (async/go
+    (let [c (async/chan 1)
+          name (get-in req [:params :name] "World")]
+      (async/<! (async/timeout 2000))
+      (async/>! c {:status 200
+                   :body (str "Hello, " name "!")
+                   :headers {}}))))
 
 (defroutes routes
   [[["/"
