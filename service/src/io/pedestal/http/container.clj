388484678a06e4e4bddd857c2d28aedbb945466d
Paul deGrandis <paul.degrandis@gmail.com>
2014-08-29 13:56:48 -0400

    There should be protocols behind container specific functionality

---
+++ service/src/io/pedestal/http/container.clj
@@ -0,0 +1,23 @@
+; Copyright 2014 Cognitect, Inc.
+
+; The use and distribution terms for this software are covered by the
+; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0)
+; which can be found in the file epl-v10.html at the root of this distribution.
+;
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+;
+; You must not remove this notice, or any other, from this software.
+
+(ns io.pedestal.http.container
+  "Deeper Pedestal<->Container integration and cooperation for HTTP handling")
+
+;; TODO: This should be its own module, unless Tim says it's fine for jetty to directly depend on Service
+
+;; Below are protocols and functions used to tune, tweak, and enhance Pedestal
+;; with Container/Servlet Engine specific hooks.
+;; The most common case is to extend integration beyond the Servlet Spec -
+;; for example, integrating and utilizing NIO throughout the entire stack.
+(defprotocol WriteByteChannelBody
+  (write-byte-channel-body [servlet-response body resume-chan context]))
+
