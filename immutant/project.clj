57d08b9306239e17015cf2e7718c9b7017aaa591
Jim Crossley <jim@crossleys.org>
2014-09-23 11:08:23 -0400

    Bump to latest incremental

--- immutant/project.clj
+++ immutant/project.clj
@@ -12,7 +12,7 @@
 (defproject io.pedestal/pedestal.immutant "0.3.1-SNAPSHOT"
   :description "Embedded Immutant adapter for Pedestal HTTP Service"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.immutant/web "2.x.incremental.303"]]
+                 [org.immutant/web "2.x.incremental.305"]]
   :min-lein-version "2.0.0"
   :global-vars {*warn-on-reflection* true}
   :repositories [["Immutant 2.x incremental builds"
