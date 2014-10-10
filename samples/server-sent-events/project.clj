63762ba54c1cc5a3cc294bee0b4f20a5c15bfcb4
Paul deGrandis <paul.degrandis@gmail.com>
2014-10-10 07:23:01 -0400

    Update Pedestal version in SSE sample

--- samples/server-sent-events/project.clj
+++ samples/server-sent-events/project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [io.pedestal/pedestal.service "0.3.1-SNAPSHOT"]
+                 [io.pedestal/pedestal.service "0.3.1"]
 
                  ;; Remove this line and uncomment the next line to
                  ;; use Tomcat instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.3.1-SNAPSHOT"]
+                 [io.pedestal/pedestal.jetty "0.3.1"]
                  ;; [io.pedestal/pedestal.tomcat "0.1.2"]
 
                  ;; Logging
