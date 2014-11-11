45a91bc4004c2df295e73ad6bfa7ca60c4b8df16
Ben Vandgrift and Russ Olsen <pair+bv+ro@cognitect.com>
2014-11-11 16:41:55 +0000

    replace comments in main-

--- samples/ring-middleware/src/ring_middleware/service.clj
+++ samples/ring-middleware/src/ring_middleware/service.clj
@@ -82,8 +82,26 @@
 ;; Consumed by ring-middleware.server/create-server
 ;; See bootstrap/default-interceptors for additional options you can configure
 (def service {:env :prod
+              ;; You can bring your own non-default interceptors. Make
+              ;; sure you include routing and set it up right for
+              ;; dev-mode. If you do, many other keys for configuring
+              ;; default interceptors will be ignored.
+              ;; :bootstrap/interceptors []
               ::bootstrap/routes routes
+
+              ;; Uncomment next line to enable CORS support, add
+              ;; string(s) specifying scheme, host and port for
+              ;; allowed source(s):
+              ;;
+              ;; "http://localhost:8080"
+              ;;
+              ;;::bootstrap/allowed-origins ["scheme://host:port"]
+
+              ;; Root for resource interceptor that is available by default.
               ::bootstrap/resource-path "/public"
+
+              ;; Either :jetty, :immutant or :tomcat (see comments in project.clj)
               ::bootstrap/type :jetty
+              ;;::bootstrap/host "localhost"
               ::bootstrap/port 8080})
 
