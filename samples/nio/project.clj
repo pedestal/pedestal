eb431408d7664a6d948011cc2ee16abf776cb77a
Ben Vandgrift <ben@vandgrift.com>
2014-11-13 13:46:12 -0500

    add licenses

--- samples/nio/project.clj
+++ samples/nio/project.clj
@@ -1,3 +1,15 @@
+; Copyright 2013 Relevance, Inc.
+; Copyright 2014 Cognitect, Inc.
+
+; The use and distribution terms for this software are covered by the
+; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0)
+; which can be found in the file epl-v10.html at the root of this distribution.
+;
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+;
+; You must not remove this notice, or any other, from this software.
+
 (defproject nio "0.0.1-SNAPSHOT"
   :description "a pedestal sample taking advantage of NIO with ByteBuffer"
   :url "https://github.com/pedestal/pedestal/tree/master/samples/nio"
