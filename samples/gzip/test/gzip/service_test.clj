4235c4c765095affcc121a764140251f6f60ef8a
Relevance Pair <pair@thinkrelevance.com>
2014-11-14 18:08:58 +0000

    real test? sure, why not.

--- samples/gzip/test/gzip/service_test.clj
+++ samples/gzip/test/gzip/service_test.clj
@@ -1,46 +1,45 @@
-; Copyright 2013 Relevance, Inc.
-; Copyright 2014 Cognitect, Inc.
-
-; The use and distribution terms for this software are covered by the
-; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0)
-; which can be found in the file epl-v10.html at the root of this distribution.
-;
-; By using this software in any fashion, you are agreeing to be bound by
-; the terms of this license.
-;
-; You must not remove this notice, or any other, from this software.
-
 (ns gzip.service-test
-  (:require [clojure.test :refer :all]
+  (:require [clj-http.client :as http]
+            [clojure.test :refer :all]
             [io.pedestal.test :refer :all]
             [io.pedestal.http :as bootstrap]
-            [gzip.service :as service]))
+            [io.pedestal.http.jetty :as jetty]
+            [io.pedestal.http.servlet :as servlet]
+            [io.pedestal.http.impl.servlet-interceptor :as incept]
+            [gzip.service :as service]
+            [gzip.server :as server]))
 
 (def service
   (::bootstrap/service-fn (bootstrap/create-servlet service/service)))
 
+(defn jetty-server
+  [app opts]
+  (prn :jetty-server :enter)
+  (let [options (assoc opts :join? false)
+        service-fn (incept/http-interceptor-service-fn [app])
+        jetty-server (servlet/servlet :service service-fn)]
+    (prn :jetty-server :building)
+    (jetty/server jetty-server opts)))
+
+(defn get-response [addy]
+  (try
+    (http/get addy)
+    (catch clojure.lang.ExceptionInfo ex
+      (prn "BOOM!\n\n")
+      (prn ex)
+      {:body "BOOM"})))
+
 (deftest home-page-test
-  (is (=
-       (:body (response-for service :get "/"))
-       "Hello World!"))
-  (is (=
-       (:headers (response-for service :get "/"))
-       {"Content-Type" "text/html;charset=UTF-8"
-        "Strict-Transport-Security" "max-age=31536000; includeSubdomains"
-        "X-Frame-Options" "DENY"
-        "X-Content-Type-Options" "nosniff"
-        "X-XSS-Protection" "1; mode=block"})))
+  (let [jetty (server/run-dev)
+        response (get-response "http://localhost:8080")
+        _ (bootstrap/stop jetty)]
+    (testing "service response"
+      (is (=
+           (:body response)
+           "Hello World!")))
+    (testing "gzip-encoded"
+      (is (.startsWith
+           (:orig-content-encoding response)
+           "gzip")))))
 
-
-(deftest about-page-test
-  (is (.contains
-       (:body (response-for service :get "/about"))
-       "Clojure 1.6"))
-  (is (=
-       (:headers (response-for service :get "/about"))
-       {"Content-Type" "text/html;charset=UTF-8"
-        "Strict-Transport-Security" "max-age=31536000; includeSubdomains"
-        "X-Frame-Options" "DENY"
-        "X-Content-Type-Options" "nosniff"
-        "X-XSS-Protection" "1; mode=block"})))
 
