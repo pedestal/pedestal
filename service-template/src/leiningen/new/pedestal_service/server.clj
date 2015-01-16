002838599b7dc0eba33fbba55cb80f89fffb2c08
Paul deGrandis <paul.degrandis@gmail.com>
2015-01-16 13:08:59 -0500

    The template should provide instructions on hooking up additional code needed for WAR deploymet

--- service-template/src/leiningen/new/pedestal_service/server.clj
+++ service-template/src/leiningen/new/pedestal_service/server.clj
@@ -32,3 +32,22 @@
   (println "\nCreating your server...")
   (server/start runnable-service))
 
+;; If you package the service up as a WAR,
+;; some form of the following function sections is required (for io.pedestal.servlet.ClojureVarServlet).
+
+;;(defonce servlet  (atom nil))
+;;
+;;(defn servlet-init
+;;  [_ config]
+;;  ;; Initialize your app here.
+;;  (reset! servlet  (server/servlet-init service/service nil)))
+;;
+;;(defn servlet-service
+;;  [_ request response]
+;;  (server/servlet-service @servlet request response))
+;;
+;;(defn servlet-destroy
+;;  [_]
+;;  (server/servlet-destroy @servlet)
+;;  (reset! servlet nil))
+
