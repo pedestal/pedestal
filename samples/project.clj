7d29b0128bc78b289b95d4ce7f44300478fc6b61
Ryan Neufeld <ryan@rkn.io>
2015-11-04 09:22:52 -0600

    Prepare 0.4.1 release

--- samples/project.clj
+++ samples/project.clj
@@ -10,7 +10,7 @@
 ;
 ; You must not remove this notice, or any other, from this software.
 
-(defproject io.pedestal/samples "0.4.1-SNAPSHOT"
+(defproject io.pedestal/samples "0.4.1"
   :plugins [[lein-sub "0.2.3"]]
   :sub ["hello-world"
         "server-sent-events"
--- service-tools/project.clj
+++ service-tools/project.clj
@@ -10,14 +10,14 @@
 ;
 ; You must not remove this notice, or any other, from this software.
 
-(defproject io.pedestal/pedestal.service-tools "0.4.1-SNAPSHOT"
+(defproject io.pedestal/pedestal.service-tools "0.4.1"
   :description "Pedestal tools for service development"
   :url "https://github.com/pedestal/pedestal"
   :scm "https://github.com/pedestal/pedestal"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[io.pedestal/pedestal.service "0.4.1-SNAPSHOT"]
+  :dependencies [[io.pedestal/pedestal.service "0.4.1"]
 
                  ;; Auto-reload changes
                  [ns-tracker "0.3.0"]
