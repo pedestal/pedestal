c3afec3cb1de94c120caea6072f0d69831645619
Paul deGrandis <paul.degrandis@gmail.com>
2016-03-18 15:53:25 -0400

    Update service-template: Correct routes, correct run-dev, enable h2c

--- service-template/src/leiningen/new/pedestal_service/service.clj
+++ service-template/src/leiningen/new/pedestal_service/service.clj
@@ -51,5 +51,12 @@
               ;; Either :jetty, :immutant or :tomcat (see comments in project.clj)
               ::http/type :jetty
               ;;::http/host "localhost"
-              ::http/port 8080})
+              ::http/port 8080
+              ::http/container-options {:h2c? true
+                                        :h2? false
+                                        ;:keystore "test/hp/keystore.jks"
+                                        ;:key-password "password"
+                                        ;:ssl-port 8443
+                                        :ssl? false}})
+
 
