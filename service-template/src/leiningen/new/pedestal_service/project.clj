2250989a64a2c8538f64109f2bb5d0000d21e12f
Paul deGrandis <paul.degrandis@gmail.com>
2016-04-19 12:23:47 -0400

    Bump logging deps in the template

--- service-template/src/leiningen/new/pedestal_service/project.clj
+++ service-template/src/leiningen/new/pedestal_service/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [io.pedestal/pedestal.service "0.4.2-SNAPSHOT"]
 
                  ;; Remove this line and uncomment one of the next lines to
@@ -12,10 +12,10 @@
                  ;; [io.pedestal/pedestal.immutant "0.4.2-SNAPSHOT"]
                  ;; [io.pedestal/pedestal.tomcat "0.4.2-SNAPSHOT"]
 
-                 [ch.qos.logback/logback-classic "1.1.3" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.12"]
-                 [org.slf4j/jcl-over-slf4j "1.7.12"]
-                 [org.slf4j/log4j-over-slf4j "1.7.12"]]
+                 [ch.qos.logback/logback-classic "1.1.7" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.13"]
+                 [org.slf4j/jcl-over-slf4j "1.7.13"]
+                 [org.slf4j/log4j-over-slf4j "1.7.13"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "{{namespace}}.server/run-dev"]}
