3483a6ce5067386e596577351e030c1e9fb8cdba
Paul deGrandis <paul.degrandis@gmail.com>
2016-04-21 16:07:35 -0400

    Provide pluggable request tooling for providers

--- service/src/io/pedestal/http/request/zerocopy.clj
+++ service/src/io/pedestal/http/request/zerocopy.clj
@@ -1,4 +1,5 @@
-(ns io.pedestal.http.impl.zerocopy-request
+(ns io.pedestal.http.request.zerocopy
+  (:require [io.pedestal.http.request :as request])
   (:import (java.util Map)))
 
 
@@ -11,89 +12,43 @@
 ;; No data is copied or cached, but all lookups involve an additional stackframe
 ;; (which may be optimized out by the JIT)
 
-(defprotocol ProxyDatastructure
-  (realized [this] "Return fully-realized version of underlying data structure."))
-(extend-protocol ProxyDatastructure
-  nil
-  (realized [t] nil))
 
-(defprotocol ContainerRequest
-  (server-port [x])
-  (server-name [x])
-  (remote-addr [x])
-  (uri [x])
-  (query-string [x])
-  (scheme [x])
-  (request-method [x])
-  (protocol [x])
-  (headers [x])
-  (ssl-client-cert [x])
-  (body [x])
-  (context [x])
-  (path-info [x]))
-(extend-protocol ContainerRequest
-  nil
-  (server-port [x] nil)
-  (server-name [x] nil)
-  (remote-addr [x] nil)
-  (uri [x] nil)
-  (query-string [x] nil)
-  (scheme [x] nil)
-  (request-method [x] nil)
-  (protocol [x] nil)
-  (headers [x] nil)
-  (ssl-client-cert [x] nil)
-  (body [x] nil)
-  (context [x] nil)
-  (path-info [x] nil))
-
-(def ring-dispatch
-  {:server-port server-port
-   :server-name server-name
-   :remote-addr remote-addr
-   :uri uri
-   :query-string query-string
-   :scheme scheme
-   :request-method request-method
-   :headers headers
-   :ssl-client-cert ssl-client-cert
-   :body body})
-
-(def nil-fn (constantly nil))
-
+;; This request doesn't copy any data from the container,
+;; and proxies all Ring-like map lookups to the containers request data structure.
+;; Care is given to uphold all value semantics.
 
 (deftype CallThroughRequest [base-request ;; This should be a ContainerRequest
                              ^clojure.lang.IPersistentMap user-data]
 
   clojure.lang.ILookup
-  ;; Upon lookup, transparently deref delayed values.
+  ;; Upon lookup, transparently dispatch to the container's request.
   (valAt [this k]
-    (user-data k ((ring-dispatch k nil-fn) base-request)))
+    (user-data k ((request/ring-dispatch k request/nil-fn) base-request)))
   (valAt [this k not-found]
     (let [v (user-data k ::not-found)]
       (if (= v ::not-found)
-        (let [v (ring-dispatch k ::not-found)]
+        (let [v (request/ring-dispatch k ::not-found)]
           (if (= v ::not-found)
             not-found
             (v base-request)))
         v)))
 
-  ProxyDatastructure
+  request/ProxyDatastructure
   (realized [this]
     (persistent!
       (reduce-kv (fn [m k v]
                    (assoc! m k (v base-request)))
                  (transient user-data)
-                 ring-dispatch)))
+                 request/ring-dispatch)))
 
   clojure.lang.Associative
   (containsKey [this k]
-    (or (contains? ring-dispatch k)
+    (or (contains? request/ring-dispatch k)
         (contains? user-data k)))
   (entryAt [this k]
     (or (.entryAt user-data k)
-        (and (contains? ring-dispatch k)
-             (clojure.lang.MapEntry. k ((ring-dispatch k) base-request)))
+        (and (contains? request/ring-dispatch k)
+             (clojure.lang.MapEntry. k ((request/ring-dispatch k) base-request)))
         nil))
   (assoc [this k v]
     (CallThroughRequest. base-request (assoc user-data k v)))
@@ -102,8 +57,12 @@
   (invoke [this k] (.valAt this k))
   (invoke [this k default] (.valAt this k default))
 
+  clojure.lang.IPersistentMap
+  (without [this k]
+    (dissoc (request/realized this) k))
+
   clojure.lang.Counted
-  (count [this] (unchecked-add (if (= base-request nil) 0 (count ring-dispatch))
+  (count [this] (unchecked-add (if (= base-request nil) 0 (count request/ring-dispatch))
                                (count user-data)))
 
   clojure.lang.IPersistentCollection
@@ -117,7 +76,7 @@
 
   clojure.lang.Seqable
   (seq [this]
-    (seq (realized this)))
+    (seq (request/realized this)))
 
   java.lang.Iterable
   (iterator [this]
@@ -131,7 +90,7 @@
   (containsValue [this v]
     (contains? (set (.values this)) v))
   (entrySet [this]
-    (.entrySet (realized this)))
+    (.entrySet (request/realized this)))
   ;; Equality is java.util.Map equality, against the fully realized map
   (equals [this o]
     (if (instance? Map o)
@@ -142,19 +101,19 @@
   (isEmpty [this]
     (and (= base-request nil) (empty? user-data)))
   (keySet [this]
-    (.keySet (realized this)))
+    (.keySet (request/realized this)))
   (size [this]
     (.count this))
   (values [this]
-    (vals (realized this)))
+    (vals (request/realized this)))
   (clear [this] (throw (UnsupportedOperationException.)))
   (put [this k v] (throw (UnsupportedOperationException.)))
   (putAll [this m] (throw (UnsupportedOperationException.)))
   (remove [this k] (throw (UnsupportedOperationException.))))
 
 (defn call-through-request
-  ([req]
-   (->CallThroughRequest req {}))
-  ([req override-map]
-   (->CallThroughRequest req override-map)))
+  ([container-req]
+   (->CallThroughRequest container-req {}))
+  ([container-req override-map]
+   (->CallThroughRequest container-req override-map)))
 
