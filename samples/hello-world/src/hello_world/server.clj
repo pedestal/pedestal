adbda39049272560a081560ae9dc4c829341723f
Michael Nygard <mtnygard@gmail.com>
2016-08-18 15:21:03 -0500

    Use http/ as alias for io.pedestal.http
    - Some old conventions were on display in this sample (i.e., 'bootstrap'
      and 'server'.)

--- samples/hello-world/src/hello_world/server.clj
+++ samples/hello-world/src/hello_world/server.clj
@@ -12,12 +12,12 @@
 
 (ns hello-world.server
   (:gen-class) ; for -main method in uberjar
-  (:require [io.pedestal.http :as server]
+  (:require [io.pedestal.http :as http]
             [hello-world.service :as service]))
 
 ;; This is an adapted service map, that can be started and stopped
-;; From the REPL you can call server/start and server/stop on this service
-(defonce runnable-service (server/create-server service/service))
+;; From the REPL you can call http/start and http/stop on this service
+(defonce runnable-service (http/create-server service/service))
 
 (defn run-dev
   "The entry-point for 'lein run-dev'"
@@ -26,21 +26,20 @@
   (-> service/service ;; start with production configuration
       (merge {:env :dev
               ;; do not block thread that starts web server
-              ::server/join? false
+              ::http/join? false
               ;; Routes can be a function that resolve routes,
               ;;  we can use this to set the routes to be reloadable
-              ::server/routes #(deref #'service/routes)
+              ::http/routes #(deref #'service/routes)
               ;; all origins are allowed in dev mode
-              ::server/allowed-origins {:creds true :allowed-origins (constantly true)}})
+              ::http/allowed-origins {:creds true :allowed-origins (constantly true)}})
       ;; Wire up interceptor chains
-      server/default-interceptors
-      server/dev-interceptors
-      server/create-server
-      server/start))
+      http/default-interceptors
+      http/dev-interceptors
+      http/create-server
+      http/start))
 
 (defn -main
   "The entry-point for 'lein run'"
   [& args]
   (println "\nCreating your server...")
-  (server/start runnable-service))
-
+  (http/start runnable-service))
--- samples/hello-world/src/hello_world/service.clj
+++ samples/hello-world/src/hello_world/service.clj
@@ -11,24 +11,22 @@
 ; You must not remove this notice, or any other, from this software.
 
 (ns hello-world.service
-  (:require [io.pedestal.http :as bootstrap]
+  (:require [io.pedestal.http :as http]
             [io.pedestal.http.route :as route]
             [io.pedestal.http.body-params :as body-params]
-            [io.pedestal.http.route.definition :refer [defroutes]]
-            [ring.util.response :as ring-resp]))
+            [io.pedestal.http.route.definition :refer [defroutes]]))
 
 (defn hello-world
   [request]
   (let [name (get-in request [:params :name] "World")]
-    (ring-resp/response (str "Hello " name "!\n"))))
+    {:status 200 :body (str "Hello " name "!\n")}))
 
 (defroutes routes
-  [[["/" 
+  [[["/"
       ["/hello" {:get hello-world}]]]])
 
-(def service {:env :prod
-              ::bootstrap/routes routes
-              ::bootstrap/resource-path "/public"
-              ::bootstrap/type :jetty
-              ::bootstrap/port 8080})
-
+(def service {:env                 :prod
+              ::http/routes        routes
+              ::http/resource-path "/public"
+              ::http/type          :jetty
+              ::http/port          8080})
