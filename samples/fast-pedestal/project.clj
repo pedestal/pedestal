7419c2e3c6e211a8ec7d6370846efb6b7d50a831
Michael Nygard <mtnygard@gmail.com>
2016-09-29 10:50:12 -0500

    Update copyright notices.

--- samples/fast-pedestal/project.clj
+++ samples/fast-pedestal/project.clj
@@ -1,3 +1,15 @@
+; Copyright 2013 Relevance, Inc.
+; Copyright 2014-2016 Cognitect, Inc.
+
+; The use and distribution terms for this software are covered by the
+; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0)
+; which can be found in the file epl-v10.html at the root of this distribution.
+;
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+;
+; You must not remove this notice, or any other, from this software.
+
 (defproject fast-pedestal "0.5.1"
   :description "Demonstrate high performance Pedestal with direct Jetty APIs"
   :url "http://pedestal.io/samples/index"
--- samples/http2/project.clj
+++ samples/http2/project.clj
@@ -1,3 +1,15 @@
+; Copyright 2013 Relevance, Inc.
+; Copyright 2014-2016 Cognitect, Inc.
+
+; The use and distribution terms for this software are covered by the
+; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0)
+; which can be found in the file epl-v10.html at the root of this distribution.
+;
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+;
+; You must not remove this notice, or any other, from this software.
+
 (defproject hp "0.5.1"
   :description "HTTP2 support"
   :url "http://pedestal.io/samples/index"
