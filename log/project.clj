971d6b86c1f79afb7c90b38578ec43c165c2baf0
Paul deGrandis <paul.degrandis@gmail.com>
2017-01-09 15:11:33 -0500

    Refresh the slf4j deps

--- log/project.clj
+++ log/project.clj
@@ -17,7 +17,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ;; logging
-                 [org.slf4j/slf4j-api "1.7.21"]
+                 [org.slf4j/slf4j-api "1.7.22"]
                  ;; metrics
                  [io.dropwizard.metrics/metrics-core "3.1.2"]]
   :min-lein-version "2.0.0"
--- service-template/src/leiningen/new/pedestal_service/project.clj
+++ service-template/src/leiningen/new/pedestal_service/project.clj
@@ -12,10 +12,10 @@
                  ;; [io.pedestal/pedestal.immutant "0.5.2-SNAPSHOT"]
                  ;; [io.pedestal/pedestal.tomcat "0.5.2-SNAPSHOT"]
 
-                 [ch.qos.logback/logback-classic "1.1.7" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.21"]
-                 [org.slf4j/jcl-over-slf4j "1.7.21"]
-                 [org.slf4j/log4j-over-slf4j "1.7.21"]]
+                 [ch.qos.logback/logback-classic "1.1.8" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.22"]
+                 [org.slf4j/jcl-over-slf4j "1.7.22"]
+                 [org.slf4j/log4j-over-slf4j "1.7.22"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
