a0e9164c9e90d9c7ac9c403e8ef34f5d3f4073a3
Paul deGrandis <paul.degrandis@gmail.com>
2018-05-16 15:24:45 -0400

    There should be a distributed tracing interceptor built against the pedestal.log protocols

--- interceptor/src/io/pedestal/interceptor/trace.clj
+++ interceptor/src/io/pedestal/interceptor/trace.clj
@@ -1,29 +1,49 @@
 (ns io.pedestal.interceptor.trace
   (:require [io.pedestal.interceptor :as interceptor]
+            [io.pedestal.interceptor.chain :as chain]
             [io.pedestal.log :as log])
   (:import (io.opentracing Tracer
                            SpanContext)
            (io.opentracing.propagation Format$Builtin
                                        TextMapExtractAdapter)))
 
-(def should-check-servlet?
-  (try (do (import 'javax.servlet.HttpServletRequest) true)
-       (catch Throwable _ false)))
-
 (defn default-span-resolver
-  [context]
-  (let [servlet-request (and should-check-servlet? (:servlet-request context))]
-    (or (::log/span context)
-        (when-let [span-context (and servlet-request
-                                     (.getAttribute ^javax.servlet.HttpServletRequest servlet-request "TracingFilter.activeSpanContext"))]
-          (log/span (::span-operation context "PedestalSpan") ^SpanContext span-context))
-        (when-let [span-context (.extract ^Tracer log/default-tracer
-                                          Format$Builtin/HTTP_HEADERS
-                                          (TextMapExtractAdaptor. (get-in context [:request :headers] {})))]
-          (log/span (::span-operation context "PedestalSpan") ^SpanContext span-context))
-        (when-let [xray-headers (get-in context [:request :headers "X-Amzn-Trace-Id"])]
-          ;;TODO: Amazon tracing
-          ))))
+  [context servlet-class]
+  (let [servlet-req (and servlet-class (:servlet-request context))
+        servlet-request (with-meta servlet-req {:tag servlet-class})
+        operation-name (::span-operation context "PedestalSpan")]
+    (try
+      ;; OpenTracing can throw errors when an extract fails due to no span being present (according to the docs)
+      ;; Defensively protect against span parse/extract errors,
+      ;;  and on exception, just create a new span without a parent, tagged appropriately
+      (or ;; Is there already a span in the context?
+          (::log/span context)
+          ;; Is there a span in the servlet request?
+          (when-let [span-context (and servlet-request
+                                       (.getAttribute servlet-request "TracingFilter.activeSpanContext"))]
+            (log/span operation-name ^SpanContext span-context))
+          ;; Is there a span in the headers?
+          (when-let [span-context (.extract ^Tracer log/default-tracer
+                                            Format$Builtin/HTTP_HEADERS
+                                            (TextMapExtractAdapter. (get-in context [:request :headers] {})))]
+            (log/span operation-name ^SpanContext span-context))
+          ;; Is there an AWS X-Ray specific span/segment in the servlet?
+          (when-let [span (and servlet-request
+                               (.getAttribute servlet-request "com.amazonaws.xray.entities.Entity"))]
+            (log/span operation-name span))
+          ;; Is there an AWS X-Ray specific span/segment in ther headers?
+          (when-let [xray-headers (get-in context [:request :headers "X-Amzn-Trace-Id"])]
+            ;;TODO: Amazon tracing
+            ;;
+            (log/error :msg "Found X-Ray Trace Headers, but currently not implemented")
+            nil)
+          ;; Otherwise, create a new span
+          (log/span operation-name))
+      (catch Exception e
+        ;; Something happened during decoding a Span,
+        ;; Create a new span and tag it accordingly
+        (log/tag-span (log/span operation-name) :revolver-exception (.getMessage e))))))
+
 
 (defn tracing-interceptor
   "Return an Interceptor for automatically initiating a distributed trace
@@ -43,7 +63,8 @@
                     1. Pedestal tracing values in the Context
                     2. OpenTracing Servlet values (if the Servlet API class is detected)
                     3. OpenTracing Header values
-                    4. AWS X-Ray Header values
+                    4. AWS X-Ray Servlet values (if the Servlet API class is detected)
+                    5. AWS X-Ray Header values
    :trace-filter - a single-arg function that is given the context and
                    returns true if a span should be created for this request.
                    If not set or set to nil, spans are created for every request
@@ -61,17 +82,29 @@
          :or {span-resolver default-span-resolver
               trace-filter (fn [ctx] true)
               uri-as-span-operation? true
-              default-span-operation "PedestalSpan"}} opts]
+              default-span-operation "PedestalSpan"}} opts
+        servlet-class (try (Class/forName "javax.servlet.HttpServletRequest")
+                           (catch Exception _ nil))]
     (interceptor/interceptor
       {:enter (fn [context]
                 (if-let [span (and (trace-filter context)
                                    (span-resolver (assoc context
                                                          ::span-operation (if uri-as-span-operation?
                                                                            (get-in context [:request :uri] default-span-operation)
-                                                                           default-span-operation))))]
-                  (assoc context
-                         ::log/span (doto span))
+                                                                           default-span-operation))
+                                                  servlet-class))]
+                  (assoc context ::log/span (log/tag-span
+                                              span
+                                              {"http.method" (get-in context [:request :request-method])
+                                               "http.url" (get-in context [:request :uri])
+                                               "http.user-agent" (get-in context [:request :header "User-Agent"])}))
                   context))
        :leave (fn [context]
-                (if-let [span (::log/span context)]))})))
+                (if-let [span (::log/span context)]
+                  (log/tag-span span "http.status_code" (get-in context [:response :status]))
+                  context))
+       :error (fn [context]
+                (if-let [span (::log/span context)]
+                  (log/log-span (::chain/error context))
+                  context))})))
 
