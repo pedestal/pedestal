06cd3cd9f4edc22150c037778cde2d4197f8dc7b
Linus Ericsson <linus.ericsson@agical.se>
2021-10-18 20:46:17 +0200

    upgrade deps for chain-provider

--- samples/chain-providers/project.clj
+++ samples/chain-providers/project.clj
@@ -10,27 +10,27 @@
 ;
 ; You must not remove this notice, or any other, from this software.
 
-(defproject chain-providers "0.5.7"
+(defproject chain-providers "0.5.9"
   :description "Demonstration of alternative chain providers in Pedestal"
   :url "http://pedestal.io/samples/index"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [io.pedestal/pedestal.service "0.5.7"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [io.pedestal/pedestal.service "0.5.9"]
 
                  ;; Remove this line and uncomment one of the next lines to
                  ;; use Immutant or Tomcat instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.5.7"]
-                 ;; [io.pedestal/pedestal.immutant "0.5.1"]
-                 ;; [io.pedestal/pedestal.tomcat "0.5.1"]
+                 [io.pedestal/pedestal.jetty "0.5.9"]
+                 ;; [io.pedestal/pedestal.immutant "0.5.9"]
+                 ;; [io.pedestal/pedestal.tomcat "0.5.9"]
 
-                 [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.26"]
-                 [org.slf4j/jcl-over-slf4j "1.7.26"]
-                 [org.slf4j/log4j-over-slf4j "1.7.26"]]
+                 [ch.qos.logback/logback-classic "1.2.6" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.32"]
+                 [org.slf4j/jcl-over-slf4j "1.7.32"]
+                 [org.slf4j/log4j-over-slf4j "1.7.32"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "chain-providers.server/run-dev"]}
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.7"]]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.9"]]}
              :uberjar {:aot [chain-providers.server]}}
   :main ^{:skip-aot true} chain-providers.server)
