83325372c2d23672e79a2eae94483e1137175ba0
Linus Ericsson <linus.ericsson@agical.se>
2021-10-18 22:43:23 +0200

    upgrade deps for tracing-interceptor-aws

--- samples/tracing-interceptor-aws/project.clj
+++ samples/tracing-interceptor-aws/project.clj
@@ -3,31 +3,30 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [io.pedestal/pedestal.service "0.5.4"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [io.pedestal/pedestal.service "0.5.9"]
 
                  ;; Remove this line and uncomment one of the next lines to
                  ;; use Immutant or Tomcat instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.5.4"]
+                 [io.pedestal/pedestal.jetty "0.5.9"]
                  ;; [io.pedestal/pedestal.immutant "0.5.4"]
                  ;; [io.pedestal/pedestal.tomcat "0.5.4"]
 
-                 [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.25"]
-                 [org.slf4j/jcl-over-slf4j "1.7.25"]
-                 [org.slf4j/log4j-over-slf4j "1.7.25"]
+                 [ch.qos.logback/logback-classic "1.2.6" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.32"]
+                 [org.slf4j/jcl-over-slf4j "1.7.32"]
+                 [org.slf4j/log4j-over-slf4j "1.7.32"]
 
                  ;; X-Ray Tracing support
-                 [io.pedestal/pedestal.aws "0.5.4"]
+                 [io.pedestal/pedestal.aws "0.5.9"]
 
                  ;; Tracing HTTP client to test propagation
-                 [com.amazonaws/aws-xray-recorder-sdk-apache-http "1.3.1"]]
+                 [com.amazonaws/aws-xray-recorder-sdk-apache-http "2.9.1"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
-  ;:java-agents [[org.mortbay.jetty.alpn/jetty-alpn-agent "2.0.5"]]
+  ;;:java-agents [[org.mortbay.jetty.alpn/jetty-alpn-agent "2.0.10"]]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "tracing-interceptor.server/run-dev"]}
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.4"]]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.9"]]}
              :uberjar {:aot [tracing-interceptor.server]}}
   :main ^{:skip-aot true} tracing-interceptor.server)
-
