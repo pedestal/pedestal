b117278db3ca54caca3232d4d9b13c6f7a503300
Linus Ericsson <linus.ericsson@agical.se>
2021-10-18 20:00:07 +0200

    json-api deps upgrade
    testing with lein run-dev.
    
    did not test jetty-alpn-agent 2.0.10.

--- samples/json-api/project.clj
+++ samples/json-api/project.clj
@@ -1,26 +1,26 @@
-(defproject json-api "0.5.5"
+(defproject json-api "0.5.9"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [io.pedestal/pedestal.service "0.5.5"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [io.pedestal/pedestal.service "0.5.9"]
 
                  ;; Remove this line and uncomment one of the next lines to
                  ;; use Immutant or Tomcat instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.5.5"]
-                 ;; [io.pedestal/pedestal.immutant "0.5.4"]
-                 ;; [io.pedestal/pedestal.tomcat "0.5.4"]
+                 [io.pedestal/pedestal.jetty "0.5.9"]
+                 ;; [io.pedestal/pedestal.immutant "0.5.9"]
+                 ;; [io.pedestal/pedestal.tomcat "0.5.9"]
 
-                 [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.25"]
-                 [org.slf4j/jcl-over-slf4j "1.7.25"]
-                 [org.slf4j/log4j-over-slf4j "1.7.25"]]
+                 [ch.qos.logback/logback-classic "1.2.6" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.32"]
+                 [org.slf4j/jcl-over-slf4j "1.7.32"]
+                 [org.slf4j/log4j-over-slf4j "1.7.32"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
-  ;:java-agents [[org.mortbay.jetty.alpn/jetty-alpn-agent "2.0.5"]]
+  ;;:java-agents [[org.mortbay.jetty.alpn/jetty-alpn-agent "2.0.10"]]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "json-api.server/run-dev"]}
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.5"]]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.9"]]}
              :uberjar {:aot [json-api.server]}}
   :main ^{:skip-aot true} json-api.server)
