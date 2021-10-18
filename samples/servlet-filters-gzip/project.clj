9b5081a7996bb4ef14e78d5d0d5181ca1fc1f86f
Linus Ericsson <linus.ericsson@agical.se>
2021-10-18 22:07:47 +0200

    upgrade deps for servlet-filters-gzip

--- samples/servlet-filters-gzip/project.clj
+++ samples/servlet-filters-gzip/project.clj
@@ -10,23 +10,23 @@
 ;
 ; You must not remove this notice, or any other, from this software.
 
-(defproject gzip "0.5.5"
+(defproject gzip "0.5.9"
   :description "a sample demonstrating container-specific configuration"
   :url "https://pedestal.io/samples/index"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [io.pedestal/pedestal.service "0.5.5"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [io.pedestal/pedestal.service "0.5.9"]
 
                  ;; This samples is specific to jetty, so
                  ;; other options don't appear here.
-                 [io.pedestal/pedestal.jetty "0.5.5"]
-                 [org.eclipse.jetty/jetty-servlets "9.4.10.v20180503"]
-                 [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.25"]
-                 [org.slf4j/jcl-over-slf4j "1.7.25"]
-                 [org.slf4j/log4j-over-slf4j "1.7.25"]
-                 [clj-http "2.0.0"]]
+                 [io.pedestal/pedestal.jetty "0.5.9"]
+                 [org.eclipse.jetty/jetty-servlets "9.4.35.v20201120"] ;;same as jetty in 0.5.9
+                 [ch.qos.logback/logback-classic "1.2.6" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.32"]
+                 [org.slf4j/jcl-over-slf4j "1.7.32"]
+                 [org.slf4j/log4j-over-slf4j "1.7.32"]
+                 [clj-http "3.12.3"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "gzip.server/run-dev"]}}}
