4eedfc5782d21ec2e11541f6aca85da6ab341185
Linus Ericsson <linus.ericsson@agical.se>
2021-10-18 21:40:06 +0200

    upgrade deos for jetty-web-sockets
    removed the explicit core-async dependency.

--- samples/jetty-web-sockets/project.clj
+++ samples/jetty-web-sockets/project.clj
@@ -10,25 +10,24 @@
 ;
 ; You must not remove this notice, or any other, from this software.
 
-(defproject jetty-web-sockets "0.5.5"
+(defproject jetty-web-sockets "0.5.9"
   :description "Sample of web sockets with Jetty"
   :url "http://pedestal.io/samples/index"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [io.pedestal/pedestal.service "0.5.5"]
-                 [org.clojure/core.async "0.4.474"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [io.pedestal/pedestal.service "0.5.9"]
 
                  ;; Remove this line and uncomment one of the next lines to
                  ;; use Immutant or Tomcat instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.5.5"]
+                 [io.pedestal/pedestal.jetty "0.5.9"]
                  ;; [io.pedestal/pedestal.immutant "0.5.5"]
                  ;; [io.pedestal/pedestal.tomcat "0.5.5"]
 
-                 [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.25"]
-                 [org.slf4j/jcl-over-slf4j "1.7.25"]
-                 [org.slf4j/log4j-over-slf4j "1.7.25"]]
+                 [ch.qos.logback/logback-classic "1.2.6" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.32"]
+                 [org.slf4j/jcl-over-slf4j "1.7.32"]
+                 [org.slf4j/log4j-over-slf4j "1.7.32"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :pedantic? :abort
