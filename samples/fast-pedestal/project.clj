48be6b2ad2c2b6113f309859f7350c2e85e78367
Linus Ericsson <linus.ericsson@agical.se>
2021-10-18 20:09:15 +0200

    update deps for fast-pedestal

--- samples/fast-pedestal/project.clj
+++ samples/fast-pedestal/project.clj
@@ -10,22 +10,22 @@
 ;
 ; You must not remove this notice, or any other, from this software.
 
-(defproject fast-pedestal "0.5.7"
+(defproject fast-pedestal "0.5.9"
   :description "Demonstrate high performance Pedestal with direct Jetty APIs"
   :url "http://pedestal.io/samples/index"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [io.pedestal/pedestal.service "0.5.7"]
-                 [io.pedestal/pedestal.jetty "0.5.7"]
-                 [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.26"]
-                 [org.slf4j/jcl-over-slf4j "1.7.26"]
-                 [org.slf4j/log4j-over-slf4j "1.7.26"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [io.pedestal/pedestal.service "0.5.9"]
+                 [io.pedestal/pedestal.jetty "0.5.9"]
+                 [ch.qos.logback/logback-classic "1.2.6" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.32"]
+                 [org.slf4j/jcl-over-slf4j "1.7.32"]
+                 [org.slf4j/log4j-over-slf4j "1.7.32"]]
   :min-lein-version "2.0.0"
-  :global-vars {;*warn-on-reflection* true
-                ;*unchecked-math* :warn-on-boxed
-                ;*compiler-options* {:disable-locals-clearing true}
+  :global-vars {;;*warn-on-reflection* true
+                ;;*unchecked-math* :warn-on-boxed
+                ;;*compiler-options* {:disable-locals-clearing true}
                 *assert* true}
   :pedantic? :abort
   :resource-paths ["config", "resources"]
