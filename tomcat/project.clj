69ef06f0459060c78a10c845f784f57024d1c2ad
Daniel De Aguiar <ddeaguiar@gmail.com>
2022-01-12 11:36:35 -0500

    Begin 0.5.11-SNAPSHOT development.

--- tomcat/project.clj
+++ tomcat/project.clj
@@ -10,7 +10,7 @@
 ;
 ; You must not remove this notice, or any other, from this software.
 
-(defproject io.pedestal/pedestal.tomcat "0.5.10"
+(defproject io.pedestal/pedestal.tomcat "0.5.11-SNAPSHOT"
   :description "Embedded Tomcat adapter for Pedestal HTTP Service"
   :url "https://github.com/pedestal/pedestal"
   :scm "https://github.com/pedestal/pedestal"
