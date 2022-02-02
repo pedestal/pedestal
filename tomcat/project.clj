0ebd46395c624d7a3bb9c44ca88043cb5adefa62
Philipp Meier <philipp.meier@nubank.com.br>
2022-02-02 14:25:07 +0100

    Bump tomcat to 9.0.58. Fixes #704

--- tomcat/project.clj
+++ tomcat/project.clj
@@ -17,9 +17,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.apache.tomcat.embed/tomcat-embed-logging-juli "8.0.52"]
-                 [org.apache.tomcat.embed/tomcat-embed-jasper "9.0.8"]
-                 [org.apache.tomcat.embed/tomcat-embed-core "9.0.8"]
+                 [org.apache.tomcat.embed/tomcat-embed-jasper "9.0.58"]
+                 [org.apache.tomcat.embed/tomcat-embed-core "9.0.58"]
                  [javax.servlet/javax.servlet-api "3.1.0"]]
   :min-lein-version "2.0.0"
   :global-vars {*warn-on-reflection* true}
