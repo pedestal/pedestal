50fe5ea89108998ac5a79a02a44432fd111ea6f8
Daniel De Aguiar <ddeaguiar@gmail.com>
2022-03-01 10:27:00 -0500

    Remove `defroutes` from 'Hello World' sample.
    Pulled from PR #668 since PR comments not addressed.

--- samples/hello-world/src/hello_world/service.clj
+++ samples/hello-world/src/hello_world/service.clj
@@ -11,19 +11,15 @@
 ; You must not remove this notice, or any other, from this software.
 
 (ns hello-world.service
-  (:require [io.pedestal.http :as http]
-            [io.pedestal.http.route :as route]
-            [io.pedestal.http.body-params :as body-params]
-            [io.pedestal.http.route.definition :refer [defroutes]]))
+  (:require [io.pedestal.http :as http]))
 
 (defn hello-world
   [request]
   (let [name (get-in request [:params :name] "World")]
     {:status 200 :body (str "Hello " name "!\n")}))
 
-(defroutes routes
-  [[["/"
-      ["/hello" {:get hello-world}]]]])
+(def routes
+  #{["/greet" :get `hello-world]})
 
 (def service {:env                 :prod
               ::http/routes        routes
