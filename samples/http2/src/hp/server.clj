458157fd750d9c374543237cf67d54e0bcfc88a9
Brian Jenkins <brian.jenkins@nubank.com.br>
2022-07-07 11:06:04 -0700

    Fix unexpanded dev routes
    Switching from defroutes to def `[...routes...] means the route is no longer
    being expanded at definition time.
    
    This doesn't matter for static routes -- the default interceptor expands them on
    startup. But routes wrapped in a function to enable "hot-reloading" aren't, so
    we need to do the expansion in the function.

--- samples/http2/src/hp/server.clj
+++ samples/http2/src/hp/server.clj
@@ -17,7 +17,7 @@
               ::server/join? false
               ;; Routes can be a function that resolve routes,
               ;;  we can use this to set the routes to be reloadable
-              ::server/routes #(deref #'service/routes)
+              ::server/routes #(route/expand-routes (deref #'service/routes))
               ;; all origins are allowed in dev mode
               ::server/allowed-origins {:creds true :allowed-origins (constantly true)}})
       ;; Wire up interceptor chains
