d1e926f61c43264a3da4b7d5f68edf9bba6b2066
Howard M. Lewis Ship <hlship@gmail.com>
2023-11-17 13:53:55 -0800

    Backport Antora documentation to the 0.6 branch

---
+++ docs/modules/guides/examples/server-sent-events/project.clj
@@ -0,0 +1,31 @@
+(defproject server-sent-events "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  ;; tag::add-hiccup[]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [io.pedestal/pedestal.service "0.6.1"]
+
+                 ;; Remove this line and uncomment one of the next lines to
+                 ;; use Immutant or Tomcat instead of Jetty:
+                 [io.pedestal/pedestal.jetty "0.6.1"]
+                 ;; [io.pedestal/pedestal.immutant "0.6.1"]
+                 ;; [io.pedestal/pedestal.tomcat "0.6.1"]
+
+                 [ch.qos.logback/logback-classic "1.2.10" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.35"]
+                 [org.slf4j/jcl-over-slf4j "1.7.35"]
+                 [org.slf4j/log4j-over-slf4j "1.7.35"]
+                 ;; Add hiccup dependency
+                 [hiccup "1.0.5"]]
+  ;; end::add-hiccup[]
+  :min-lein-version "2.0.0"
+  :resource-paths ["config", "resources"]
+  ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
+  ;:java-agents [[org.mortbay.jetty.alpn/jetty-alpn-agent "2.0.5"]]
+  :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "server-sent-events.server/run-dev"]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.6.1"]]}
+             :uberjar {:aot [server-sent-events.server]}}
+  :main ^{:skip-aot true} server-sent-events.server)
+
---
+++ docs/modules/guides/examples/todo-api/src/main-complete.clj
@@ -0,0 +1,157 @@
+(ns main
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.test :as test]))
+
+(defn response [status body & {:as headers}]
+  {:status status :body body :headers headers})
+
+(def ok (partial response 200))
+(def created (partial response 201))
+(def accepted (partial response 202))
+
+;;;
+;;; "Database" functions
+;;;
+(defonce database (atom {}))
+
+(defn find-list-by-id [dbval db-id]
+  (get dbval db-id))
+
+(defn find-list-item-by-ids [dbval list-id item-id]
+  (get-in dbval [list-id :items item-id] nil))
+
+(defn list-item-add
+  [dbval list-id item-id new-item]
+  (if (contains? dbval list-id)
+    (assoc-in dbval [list-id :items item-id] new-item)
+    dbval))
+
+(def db-interceptor
+  {:name :database-interceptor
+   :enter
+   (fn [context]
+     (update context :request assoc :database @database))
+   :leave
+   (fn [context]
+     (if-let [[op & args] (:tx-data context)]
+       (do
+         (apply swap! database op args)
+         (assoc-in context [:request :database] @database))
+       context))})
+
+;;;
+;;; Domain functions
+;;;
+(defn make-list [nm]
+  {:name nm
+   :items {}})
+
+(defn make-list-item [nm]
+  {:name nm
+   :done? false})
+
+;;;
+;;; API Interceptors
+;;;
+(def echo
+  {:name :echo
+   :enter
+   (fn [context]
+     (let [request (:request context)
+           response (ok context)]
+       (assoc context :response response)))})
+
+(def entity-render
+  {:name :entity-render
+   :leave
+   (fn [context]
+     (if-let [item (:result context)]
+       (assoc context :response (ok item))
+       context))})
+
+(def list-create
+  {:name :list-create
+   :enter
+   (fn [context]
+     (let [nm (get-in context [:request :query-params :name] "Unnamed List")
+           new-list (make-list nm)
+           db-id (str (gensym "l"))
+           url (route/url-for :list-view :params {:list-id db-id})]
+       (assoc context
+              :response (created new-list "Location" url)
+              :tx-data [assoc db-id new-list])))})
+
+(def list-view
+  {:name :list-view
+   :enter
+   (fn [context]
+     (let [db-id (get-in context [:request :path-params :list-id])
+           the-list (when db-id
+                      (find-list-by-id
+                        (get-in context [:request :database])
+                        db-id))]
+       (cond-> context
+         the-list (assoc context :result the-list))))})
+
+(def list-item-view
+  {:name :list-item-view
+   :leave
+   (fn [context]
+     (let [list-id (get-in context [:request :path-params :list-id])
+           item-id (and list-id
+                        (get-in context [:request :path-params :item-id]))
+           item (and item-id
+                     (find-list-item-by-ids (get-in context [:request :database]) list-id item-id))]
+       (cond-> context
+         item (assoc :result item))))})
+
+(def list-item-create
+  {:name :list-item-create
+   :enter
+   (fn [context]
+     (if-let [list-id (get-in context [:request :path-params :list-id])]
+       (let [nm (get-in context [:request :query-params :name] "Unnamed Item")
+             new-item (make-list-item nm)
+             item-id (str (gensym "i"))]
+         (-> context
+             (assoc :tx-data [list-item-add list-id item-id new-item])
+             (assoc-in [:request :path-params :item-id] item-id)))
+       context))})
+
+(def routes
+  (route/expand-routes
+    #{["/todo" :post [db-interceptor list-create]]
+      ["/todo" :get echo :route-name :list-query-form]
+      ["/todo/:list-id" :get [entity-render db-interceptor list-view]]
+      ["/todo/:list-id" :post [entity-render list-item-view db-interceptor list-item-create]]
+      ["/todo/:list-id/:item-id" :get [entity-render list-item-view db-interceptor]]
+      ["/todo/:list-id/:item-id" :put echo :route-name :list-item-update]
+      ["/todo/:list-id/:item-id" :delete echo :route-name :list-item-delete]}))
+
+(def service-map
+  {::http/routes routes
+   ::http/type :jetty
+   ::http/port 8890})
+
+(defn start []
+  (http/start (http/create-server service-map)))
+
+;; For interactive development
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server
+          (http/start (http/create-server
+                        (assoc service-map
+                               ::http/join? false)))))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
+
+(defn test-request [verb url]
+  (io.pedestal.test/response-for (::http/service-fn @server) verb url))
