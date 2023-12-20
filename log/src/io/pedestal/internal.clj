1b389beef92117f4a46c89fa838121861f46bb46
Howard M. Lewis Ship <hlship@gmail.com>
2023-12-19 16:11:14 -0800

    Allow global override of event map formatter
    Fixes #666

--- log/src/io/pedestal/internal.clj
+++ log/src/io/pedestal/internal.clj
@@ -9,8 +9,9 @@
 ;
 ; You must not remove this notice, or any other, from this software.
 
-(ns  ^:no-doc io.pedestal.internal
-  "Internal utilities, not for reuse, subject to change at any time.")
+(ns ^:no-doc io.pedestal.internal
+  "Internal utilities, not for reuse, subject to change at any time."
+  (:require [clojure.string :as str]))
 
 (defn vec-conj
   [v value]
@@ -19,3 +20,33 @@
 (defn into-vec
   [v coll]
   (into (or v []) coll))
+
+(defn println-err [& s]
+  (binding [*out* *err*]
+    (apply println s)))
+
+(defn resolve-var-from
+  [property-name env-var]
+  (let [resolver (fn [from-source from-name s]
+                   (when s
+                     (try
+                       (let [slashx     (str/index-of s "/")
+                             ns-str     (when slashx
+                                          (subs s 0 slashx))
+                             symbol-str (if slashx
+                                          (subs s (inc slashx))
+                                          s)
+                             sym        (symbol ns-str symbol-str)
+                             v          (requiring-resolve sym)]
+                         (if v
+                           @v
+                           (println-err (format "WARNING: Symbol %s (from %s %s) does not exist"
+                                                s
+                                                from-source from-name))))
+                       (catch Exception e
+                         (println-err (format "ERROR: Could not resolve symbol %s (from %s %s): %s"
+                                              s
+                                              from-source from-name
+                                              (ex-message e)))))))]
+    (or (resolver "JVM property" property-name (System/getProperty property-name))
+        (resolver "environment variable" env-var (System/getenv property-name)))))
