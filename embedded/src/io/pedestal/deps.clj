6a4ea0e3169860f04ebc236da4770d579841186e
Howard M. Lewis Ship <hlship@gmail.com>
2024-01-08 15:25:39 -0800

    Make deps-new template execution less chatty

--- embedded/src/io/pedestal/deps.clj
+++ embedded/src/io/pedestal/deps.clj
@@ -15,23 +15,20 @@
   "Example data-fn handler.
 
   Result is merged onto existing options data."
-  [data]
+  [_data]
   ;; returning nil means no changes to options data
-  (println "data-fn returning nil")
   nil)
 
 (defn template-fn
   "Example template-fn handler.
 
   Result is used as the EDN for the template."
-  [edn data]
+  [edn _data]
   ;; must return the whole EDN hash map
-  (println "template-fn returning edn")
   edn)
 
 (defn post-process-fn
   "Example post-process-fn handler.
 
   Can programmatically modify files in the generated project."
-  [edn data]
-  (println "post-process-fn not modifying" (:target-dir data)))
+  [_edn _data])
