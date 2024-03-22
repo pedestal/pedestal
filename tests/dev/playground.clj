36a4f1da33d9edbf43ba91480025f7b72ca010a5
Howard M. Lewis Ship <hlship@gmail.com>
2024-03-22 13:21:03 -0700

    Set up file access for the playground

--- tests/dev/playground.clj
+++ tests/dev/playground.clj
@@ -1,4 +1,16 @@
+; Copyright 2024 Nubank NA
+;
+; The use and distribution terms for this software are covered by the
+; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0)
+; which can be found in the file epl-v10.html at the root of this distribution.
+;
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+;
+; You must not remove this notice, or any other, from this software.
+
 (ns playground
+  "Used primarily to test telemetry using the Open Telemetry Java Agent."
   (:require [io.pedestal.metrics :as m]
             [io.pedestal.http :as http]
             [io.pedestal.http.route :as route]
@@ -57,6 +69,7 @@
   (->> {::http/port   8080
         ::http/type   :jetty
         ::http/join?  false
+        ::http/file-path "../docs"
         ::http/routes (route/routes-from routes)}
        http/default-interceptors
        http/dev-interceptors
