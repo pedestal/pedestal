012ec076e7704212b0c37d36d26c916bd899399b
Howard M. Lewis Ship <hlship@gmail.com>
2024-06-05 08:51:40 -0300

    Fill gap in code coverage of io.pedestal.tracing.spi

--- tests/test/io/pedestal/metrics/otel_test.clj
+++ tests/test/io/pedestal/metrics/otel_test.clj
@@ -14,11 +14,13 @@
             [io.pedestal.metrics :as metrics]
             [matcher-combinators.matchers :as m]
             [io.pedestal.metrics.otel :as otel]
+            [io.pedestal.tracing.spi :as tracing.spi]
             [io.pedestal.telemetry.internal :refer [convert-key]]
             [clojure.test :refer [deftest is are use-fixtures]])
   (:import (clojure.lang ExceptionInfo)
            (io.opentelemetry.api.metrics DoubleGaugeBuilder DoubleHistogramBuilder LongCounter LongCounterBuilder LongGaugeBuilder LongHistogram LongHistogramBuilder Meter
                                          ObservableLongGauge ObservableLongMeasurement)
+           (io.opentelemetry.api.trace Span SpanBuilder)
            (java.util.function Consumer)))
 
 (def *now (atom 0))
@@ -354,3 +356,6 @@
       (is (match? [[:record "histogram.test" 42 clojure-domain-attributes]]
                    (events)))))
 
+(deftest span-with-nil-tracing-source
+  (is (instance? SpanBuilder
+                 (tracing.spi/create-span nil nil nil))))
