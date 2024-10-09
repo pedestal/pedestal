b3fd6873e6cf67f65e17e8fd8f5d8a578c3a8c9d
Howard M. Lewis Ship <hlship@gmail.com>
2024-10-09 14:48:07 -0700

    Refresh dependencies

--- tests/bench/io/pedestal/sawtooth_bench.clj
+++ tests/bench/io/pedestal/sawtooth_bench.clj
@@ -1,3 +1,14 @@
+; Copyright 2024 Nubank NA
+;
+; The use and distribution terms for this software are covered by the
+; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0)
+; which can be found in the file epl-v10.html at the root of this distribution.
+;
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+;
+; You must not remove this notice, or any other, from this software.
+
 (ns io.pedestal.sawtooth-bench
   "Used to run comparisons of Sawtooth vs. prefix-tree router performance."
   (:require [clojure.string :as string]
@@ -51,7 +62,6 @@
   [batch-size router-name]
   (let [router-fn (routers router-name)]
     (run! (fn [request]
-            #_(prn request)
             (router-fn request))
           (requests batch-size))))
 
