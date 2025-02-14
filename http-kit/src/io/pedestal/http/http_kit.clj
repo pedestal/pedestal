62a693c3e0832d96cb537453b20341d3e9c1c0ab
Howard M. Lewis Ship <hlship@gmail.com>
2025-02-13 23:14:53 -0800

    Pare back http-kit code until we're ready to build it

--- http-kit/src/io/pedestal/http/http_kit.clj
+++ http-kit/src/io/pedestal/http/http_kit.clj
@@ -14,34 +14,8 @@
 
   HttpKit provides features similar to the Servlet API, including WebSockets, but does not
   implement any of the underlying interfaces."
-  (:require [io.pedestal.http :as http]
-            [org.httpkit.server :as hk-server]
-            [io.pedestal.http.impl.servlet-interceptor :as si]
-            [io.pedestal.interceptor.chain :as chain]))
+  (:require[org.httpkit.server :as hk-server]
+           [io.pedestal.interceptor.chain :as chain]))
 
 
-(defn- channelize
-  [request]
-  (hk-server/as-channel request nil))
 
-(defn- create-provider
-  [service-map]
-  (let [{::http/keys [interceptors
-                      initial-context
-                      container-options]} service-map
-        {:keys [exception-analyzer]
-         :or   {exception-analyzer si/default-exception-analyzer}} container-options
-        context (-> initial-context
-                    si/terminate-when-response
-                    (chain/enqueue [])
-                    (chain/enqueue interceptors))])
-
-  )
-
-(defn chain-provider
-  "Implements the equivalent of the [[http-interceptor-service-fn]], but for HttpKit.
-
-  You should as :chain-provider io.pedestal.http.http-kit/chain-provider to your service map."
-  [service-map]
-  (let [chain-provider (create-provider service-map)]
-    (assoc service-map ::service-fn chain-provider)))
