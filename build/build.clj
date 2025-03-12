284475a597102c1d0d3baaf6361b55e96889a762
Howard M. Lewis Ship <hlship@gmail.com>
2025-03-12 15:16:28 -0700

    Don't build and deploy empty service-template (it has been removed)

--- build/build.clj
+++ build/build.clj
@@ -176,10 +176,7 @@
   (println (str "Building version " version (when dry-run " (dry run)") " ..."))
 
   (let [build-and-install (requiring-resolve 'io.pedestal.deploy/build-and-install)
-        ;; We only care about the Leiningen service-template when either deploying, or
-        ;; when changing the version number.
-        module-dirs'      (conj module-dirs "service-template")
-        artifacts-data    (mapv #(build-and-install % version) module-dirs')]
+        artifacts-data    (mapv #(build-and-install % version) module-dirs)]
     (when-not dry-run
       (println "Deploying ...")
       (run! #(deploy-jar (assoc % :sign-key-id sign-key-id)) artifacts-data)))
