a6cfa10b4031c513f9474bb1c9682bb6e91351af
Howard M. Lewis Ship <hlship@gmail.com>
2025-04-10 18:03:08 -0700

    Rewrite the hello-world guide

--- docs/modules/guides/examples/hello/src/hello.clj
+++ docs/modules/guides/examples/hello/src/hello.clj
@@ -1,28 +1,28 @@
 ;; tag::ns[]
 (ns hello                                                   ;; <1>
-  (:require [io.pedestal.http :as http]                     ;; <2>
-            [io.pedestal.http.route :as route]))            ;; <3>
+  (:require [io.pedestal.connector :as conn]                ;; <2>
+            [io.pedestal.http.http-kit :as hk]))            ;; <3>
 ;; end::ns[]
 
 ;; tag::response[]
-(defn respond-hello [request]                               ;; <1>
-  {:status 200 :body "Hello, world!"})                      ;; <2>
+(defn hello-handler [_request]                              ;; <1>
+  {:status 200
+   :body   "Hello, world!"})                                ;; <2>
 ;; end::response[]
 
 
 ;; tag::routing[]
 (def routes
-  (route/expand-routes                                      ;; <1>
-    #{["/greet" :get respond-hello :route-name :greet]}))   ;; <2>
+  #{["/greet" :get hello-handler :route-name :greet]})
 ;; end::routing[]
 
-;; tag::server[]
-(defn create-server []
-  (http/create-server                                       ;; <1>
-    {::http/routes routes                                   ;; <2>
-     ::http/type :jetty                                     ;; <3>
-     ::http/port 8890}))                                    ;; <4>
+;; tag::connector[]
+(defn create-connector []
+  (-> (conn/default-connector-map 8890)                     ;; <1>
+      (conn/with-default-interceptors)                      ;; <2>
+      (conn/with-routes routes)                             ;; <3>
+      (hk/create-connector nil)))                           ;; <4>
 
 (defn start []
-  (http/start (create-server)))                             ;; <5>
-;; end::server[]
+  (conn/start! (create-connector)))                         ;; <5>
+;; end::connector[]
