8c6ff977643ee5f992d61a5fa18a251bad520063
Howard M. Lewis Ship <hlship@gmail.com>
2025-05-30 16:17:08 -0700

    Write end-to-end server-side event tests

--- tests/test/io/pedestal/http/sse_test.clj
+++ tests/test/io/pedestal/http/sse_test.clj
@@ -1,4 +1,4 @@
-; Copyright 2024 Nubank NA
+; Copyright 2024-2025 Nubank NA
 ; Copyright 2013 Relevance, Inc.
 ; Copyright 2014-2022 Cognitect, Inc.
 
@@ -12,25 +12,36 @@
 ; You must not remove this notice, or any other, from this software.
 
 (ns io.pedestal.http.sse-test
-  (:require [io.pedestal.interceptor.chain :as interceptor]
-            [io.pedestal.http.sse :refer [start-event-stream]]
+  (:require [io.pedestal.http.jetty :as jetty]
+            [io.pedestal.interceptor.chain :as chain]
+            [io.pedestal.interceptor :as interceptor]
+            [io.pedestal.connector :as conn]
+            [clojure.core.async :refer [go >! <! timeout close! chan put!]]
+            [io.pedestal.http.sse :as sse :refer [start-event-stream]]
+            [io.pedestal.test-common :refer [<!!?]]
             [clojure.test :refer [deftest is]]
             [io.pedestal.http.cors :as cors])
-  (:import (clojure.core.async.impl.protocols Channel)))
+  (:import (clojure.core.async.impl.protocols Channel)
+           (cloud.prefab.sse SSEHandler)
+           (cloud.prefab.sse.events DataEvent)
+           (java.net URI)
+           (java.net.http HttpClient HttpRequest HttpResponse HttpResponse$BodyHandlers)
+           (java.time Duration)
+           (java.util.concurrent Flow$Subscriber Flow$Subscription)))
 
 (deftest sse-start-stream
   (let [fake-context        {:request {:headers {"origin" "http://foo.com:8080"}}}
-        interceptor-context (interceptor/enqueue* fake-context
-                                                  (cors/allow-origin ["http://foo.com:8080"])
-                                                  ;; The `stream-ready-fn` takes the channel and the context
-                                                  (start-event-stream (fn [ch _context] ch)))
+        interceptor-context (chain/enqueue* fake-context
+                                            (cors/allow-origin ["http://foo.com:8080"])
+                                            ;; The `stream-ready-fn` takes the channel and the context
+                                            (start-event-stream (fn [ch _context] ch)))
         {{body                                          :body
           {content-type  "Content-Type"
            connection    "Connection"
            cache-control "Cache-Control"
            allow-origin  "Access-Control-Allow-Origin"} :headers
-          status                                        :status} :response
-         } (interceptor/execute interceptor-context)]
+          status                                        :status} :response}
+        (chain/execute interceptor-context)]
     (is body "Response has a body")
     (is (instance? Channel body) "Response body is a channel")
     (is (= 200 status)
@@ -43,4 +54,96 @@
         "The client is instructed not to cache the event stream")
     (is (= "http://foo.com:8080" allow-origin)
         "The origin is allowed")))
+
+(def sse-interceptor
+  (interceptor/interceptor
+    {:name  ::sse
+     :enter (fn [context]
+              (let [path-params (get-in context [:request :path-params])
+                    n           (-> path-params :count parse-long)
+                    id          (:id path-params)
+                    process-fn  (fn [ch _context]
+                                  (go
+                                    (dotimes [i n]
+                                      (<! (timeout 100))
+                                      (>! ch {:name "count"
+                                              :data (str (inc i) "...")
+                                              :id   id}))
+                                    (<! (timeout 100))
+                                    (close! ch)))]
+                (sse/start-stream process-fn
+                                  context
+                                  1)))}))
+
+(defn sse-session
+  [url]
+  (let [ch         (chan 5)
+        handler    (SSEHandler.)
+        httpClient (HttpClient/newHttpClient)
+        subscriber (reify Flow$Subscriber
+
+                     (onSubscribe [_ subscription]
+                       (.request subscription Long/MAX_VALUE))
+
+                     ;; Normally, might want to check for comment events
+                     ;; But Pedestal doesn't even send those.
+                     (onNext [_ data-event]
+                       (put! ch [:message {:name (.getEventName ^DataEvent data-event)
+                                           :data (.getData ^DataEvent data-event)
+                                           :id   (.getLastEventId ^DataEvent data-event)}]))
+
+                     (onError [_ error]
+                       (put! ch [:error error])
+                       (close! ch))
+
+                     (onComplete [_]
+                       (put! ch [:complete])
+                       (close! ch)))
+        request    (-> (HttpRequest/newBuilder)
+                       (.header "Accept" SSEHandler/EVENT_STREAM_MEDIA_TYPE)
+                       (.timeout (Duration/ofSeconds 100))
+                       (.uri (URI/create url))
+                       .build)
+        _          (.subscribe handler subscriber)
+        result     (.send httpClient request (HttpResponse$BodyHandlers/fromLineSubscriber handler))]
+    (is (= 200
+           (.statusCode ^HttpResponse result)))
+
+    ;; Just a test, don't care that HttpClient is not properly closed.
+    ch))
+
+
+(defn- new-connector
+  []
+  (-> (conn/default-connector-map 9876)
+      (conn/with-interceptor cors/dev-allow-origin)
+      conn/with-default-interceptors
+      (conn/with-routes #{["/api/sse/:count/:id" :get sse-interceptor]})))
+
+(deftest jetty-end-to-end
+  (let [conn (-> (new-connector)
+                 (jetty/create-connector nil)
+                 (conn/start!))]
+    (try
+      (let [ch (sse-session "http://localhost:9876/api/sse/3/abcde")]
+
+        (is (match? [:message {:name "count"
+                               :data "1...\n"
+                               :id   "abcde"}] (<!!? ch)))
+
+        (is (match? [:message {:name "count"
+                               :data "2...\n"
+                               :id   "abcde"}] (<!!? ch)))
+
+        (is (match? [:message {:name "count"
+                               :data "3...\n"
+                               :id   "abcde"}] (<!!? ch)))
+
+        (is (= [:complete] (<!!? ch)))
+
+        ;; And the channel is closed
+        (is (= nil (<!!? ch))))
+      (finally
+
+        (conn/stop! conn)))))
 
