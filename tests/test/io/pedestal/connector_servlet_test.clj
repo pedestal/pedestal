de1a3cb05e6fdaa2aeb32d221563009eb63c6937
Howard M. Lewis Ship <hlship@gmail.com>
2025-06-06 11:00:23 -0700

    Add an async round trip test for ConnectorServlet

--- tests/test/io/pedestal/connector_servlet_test.clj
+++ tests/test/io/pedestal/connector_servlet_test.clj
@@ -11,6 +11,7 @@
 
 (ns io.pedestal.connector-servlet-test
   (:require [clojure.test :refer [deftest is]]
+            [clojure.core.async :refer [go]]
             [io.pedestal.connector :as conn]
             [io.pedestal.http.response :refer [respond-with]]
             [io.pedestal.connector.servlet :as servlet])
@@ -21,7 +22,13 @@
 (def hello
   {:name  ::hello
    :enter (fn [context]
-            (respond-with context 200 (::message context)))})
+            (respond-with context 200 (::greeting context)))})
+
+(def hello-async
+  {:name  ::hello-async
+   :enter (fn [context]
+            (go
+              (respond-with context 200 (::salutation context))))})
 
 (defn create-bridge
   [^Servlet servlet ^ServletConfig config]
@@ -29,24 +36,34 @@
   (servlet/create-bridge servlet
                          (-> (conn/default-connector-map -1)
                              (conn/with-default-interceptors)
-                             (assoc-in [:initial-context ::message] "Greetings!")
+                             (assoc :initial-context {::greeting   "Greetings!"
+                                                      ::salutation "Salutations!"})
                              (conn/with-routes
-                               #{["/hello" :get hello]}))
+                               #{["/hello" :get hello]
+                                 ["/hello-async" :get hello-async]}))
                          nil))
 
-(deftest round-trip
-  (let [mock-state (MockState. "http://locahost:8080/hello" "GET" "http" "locahost" 8080 "hello" "" {} nil)
-        servlet    (ConnectorServlet.)
-        params     {"createBridge" "io.pedestal.connector-servlet-test/create-bridge"}
-        config     (reify ServletConfig
-                     (getInitParameter [_ k]
-                       (get params k)))
-        _          (do
-                     (.init servlet config)
-                     (.service servlet (.request mock-state) (.response mock-state)))
-        response-bytes (-> mock-state .responseStream .toByteArray)
+(defn- expect [path expected-response async?]
+  (let [mock-state      (MockState. (str "http://locahost:8080/" path) "GET" "http" "locahost" 8080 path "" {} nil)
+        servlet         (ConnectorServlet.)
+        params          {"createBridge" "io.pedestal.connector-servlet-test/create-bridge"}
+        config          (reify ServletConfig
+                          (getInitParameter [_ k]
+                            (get params k)))
+        _               (do
+                          (.init servlet config)
+                          (.service servlet (.request mock-state) (.response mock-state))
+
+                          (when async?
+                            (.waitForCompletion mock-state 100)))
+        response-bytes  (-> mock-state .responseStream .toByteArray)
         response-string (String. response-bytes "UTF-8")]
     (is (= 200 (.responseStatus mock-state)))
-    (is (= "Greetings!"
-           response-string))))
+    (is (= expected-response response-string))))
+
+(deftest round-trip
+  (expect "hello" "Greetings!" false))
+
+(deftest round-trip-async
+  (expect "hello-async" "Salutations!" true))
 
