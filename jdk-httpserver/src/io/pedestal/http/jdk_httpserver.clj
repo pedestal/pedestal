14b66db4647ace4f250a5b99a753092f5d304fa9
Enzzo Cavallo <souenzzo@gmail.com>
2025-06-07 12:27:10 -0300

    Clojure 1.11 suport

--- jdk-httpserver/src/io/pedestal/http/jdk_httpserver.clj
+++ jdk-httpserver/src/io/pedestal/http/jdk_httpserver.clj
@@ -18,12 +18,13 @@
         context-path (.getPath (.getHttpContext http-exchange))
         uri (.getPath request-uri)
         query-string (.getQuery request-uri)
-        headers (HttpExchange/.getRequestHeaders http-exchange)]
+        headers (.getRequestHeaders http-exchange)
+        https? (instance? HttpsExchange http-exchange)]
     (cond-> {:body           (.getRequestBody http-exchange)
              :headers        (into {}
                                (map (fn [[k v]]
-                                      ;; TODO: Handle multiple headers?
-                                      [k (first v)]))
+                                      ;; Follows jetty impl
+                                      [k (string/join "," v)]))
                                headers)
              :protocol       (.getProtocol http-exchange)
              :remote-addr    (-> http-exchange
@@ -34,19 +35,22 @@
                                .getRequestMethod
                                string/lower-case
                                keyword)
-             :scheme         (if (instance? HttpsExchange http-exchange)
+             :scheme         (if https?
                                :https
                                :http)
-             :server-name    (str (or (.getHost (HttpExchange/.getRequestURI http-exchange))
+             :server-name    (str (or (.getHost (.getRequestURI http-exchange))
                                     (some-> headers (.getFirst "host") (string/split #":") first)))
              :server-port    (-> http-exchange
                                .getHttpContext
                                .getServer
                                .getAddress
                                .getPort)
-             #_:ssl-client-cert
              :uri            uri
              :context-path   context-path}
+      ;; TODO: https support
+      #_#_https? (assoc :ssl-client-cert (-> ^HttpsExchange http-exchange
+                                           .getSSLSession
+                                           ->X509Certificate))
       :always (request/set-context context-path)
       query-string (assoc :query-string query-string))))
 
@@ -74,7 +78,7 @@
                (InetSocketAddress. ^String host (int port))
                (InetSocketAddress. port))
         server (HttpServer/create addr 0)]
-    (HttpServer/.createContext server context-path
+    (.createContext server context-path
       (reify HttpHandler
         (handle [_this http-exchange]
           (let [{:keys [response]} (-> initial-context
@@ -86,19 +90,19 @@
               ;; TODO: Handle collections on value?!
               (.add response-headers k (str v)))
             (if (nil? body)
-              (HttpExchange/.sendResponseHeaders http-exchange status -1)
+              (.sendResponseHeaders http-exchange status -1)
               (let [content-length (or (some-> response-headers
                                          (.getFirst "content-length")
                                          parse-long)
                                      0)]
-                (HttpExchange/.sendResponseHeaders http-exchange status content-length)
+                (.sendResponseHeaders http-exchange status content-length)
                 (with-open [response-body (.getResponseBody http-exchange)]
                   (-write-body-to-stream body response response-body))))))))
     (reify
       p/PedestalConnector
       (start-connector! [this]
-        (HttpServer/.start server)
+        (.start server)
         this)
       (stop-connector! [this]
-        (HttpServer/.stop server 0)
+        (.stop server 0)
         this))))
