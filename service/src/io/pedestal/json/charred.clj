5cad39c66c6174b78f0db2570f9f5f3467de86a4
Howard M. Lewis Ship <hlship@gmail.com>
2026-01-28 14:40:41 -0800

    Only create JSONWriter constructor fn once

--- service/src/io/pedestal/json/charred.clj
+++ service/src/io/pedestal/json/charred.clj
@@ -20,9 +20,8 @@
 (defn- write-json-noclose
   ;; Calling charred/write-json closes the writer, which doesn't work with Pedestal and Servlet API.
   ;; Instead, we create a writer via json/json-writer-fn and write the object to it.
-  [obj ^Writer stream-writer]
-  (let [writer-fn               (charred/json-writer-fn nil)
-        ^JSONWriter json-writer (writer-fn stream-writer)]
+  [obj ^Writer stream-writer writer-fn]
+  (let [^JSONWriter json-writer (writer-fn stream-writer)]
     (.writeObject json-writer obj)
     ;; The feels dirty, but some JSON could still be in the JSONWriter's internal writer
     ;; that wraps stream-writer, and that needs to be flushed but not closed, and
@@ -32,17 +31,18 @@
 (defn stream-json
   "Writes the object as JSON to the stream and returns the stream.  Some gymnastics occur to ensure
   that the stream is not closed."
-  [object ^OutputStream stream]
+  [object ^OutputStream stream writer-fn]
   (with-open [writer (io/writer stream)]
-    (write-json-noclose object writer)
+    (write-json-noclose object writer writer-fn)
     stream))
 
 (defn processor
   "Returns a JSONProcessor that functions using the charred JSON library."
   []
-  (reify p/JSONProcessor
-    (read-json [_ reader options]
-      (charred/read-json reader options))
+  (let [writer-fn (charred/json-writer-fn nil)]
+    (reify p/JSONProcessor
+      (read-json [_ reader options]
+        (charred/read-json reader options))
 
-    (stream-json [_ object stream]
-      (stream-json object stream))))
+      (stream-json [_ object stream]
+        (stream-json object stream writer-fn)))))
